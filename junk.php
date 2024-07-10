3c4fb6406d843e59319fc14bd1629c992dff8bae
Al-Hira <93406796+Al-Hira@users.noreply.github.com>
2024-07-10 14:13:35 +0530

    Create junk.php

---
+++ junk.php
@@ -0,0 +1,26 @@
+  <div class="row dev-list text-center"> 
+               
+<center>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12  wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                    <div class="widget clearfix">
+                        <img src="images/tahira.jpg" alt="" class="img-responsive img-rounded">
+                        <div class="widget-title">
+                            <h3>Tahira Rahim</h3>
+                            <small>Owner / Radiology Technician</small>
+                        </div>
+                        <!-- end title -->
+                        <p>Hello guys, I am Tahira Rahim from waskura . I am Radiology Technician and founder of Al Hira.</p>
+
+                        <div class="footer-social">
+                            <a href="https://www.instagram.com/tahirarahim.19?igsh=MXFsODJ6ZmFiMmJ6Yw==" class="btn grd1"><i class="fa fa-facebook"></i></a>
+<!--                               <a href="https://infobytes.info" class="btn grd1">Backlink</a>
+                             <a href="https://infobytes.info/index.php" class="btn grd1">Backlink</a>
+                              <a href="https://infobytes.info/single_page.php?single=38" class="btn grd1">Backlink</a> -->
+                        </div>
+                    </div>
+                    <!--widget -->
+                </div>
+                <!-- end col -->
+</center>
+
+            </div>
